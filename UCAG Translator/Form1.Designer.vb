6dedaa86e5b49adf5ac1dd934aabe2c012d7633c
lenn0n <gameoveralisa@gmail.com>
2020-07-04 14:30:21 +0800

    First commit

---
+++ UCAG Translator/Form1.Designer.vb
@@ -0,0 +1,261 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lyrics = New System.Windows.Forms.RichTextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.rna = New System.Windows.Forms.RichTextBox()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.dna = New System.Windows.Forms.RichTextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Panel3 = New System.Windows.Forms.Panel()
+        Me.Panel4 = New System.Windows.Forms.Panel()
+        Me.Panel5 = New System.Windows.Forms.Panel()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Panel1.SuspendLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'lyrics
+        '
+        Me.lyrics.Font = New System.Drawing.Font("Verdana", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lyrics.ForeColor = System.Drawing.Color.Black
+        Me.lyrics.Location = New System.Drawing.Point(66, 198)
+        Me.lyrics.Name = "lyrics"
+        Me.lyrics.Size = New System.Drawing.Size(314, 296)
+        Me.lyrics.TabIndex = 1
+        Me.lyrics.Text = ""
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Sitka Display", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.LawnGreen
+        Me.Label1.Location = New System.Drawing.Point(54, 152)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(57, 30)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Text:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Sitka Display", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.Color.LawnGreen
+        Me.Label2.Location = New System.Drawing.Point(435, 152)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(113, 30)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "RNA Value:"
+        '
+        'rna
+        '
+        Me.rna.Cursor = System.Windows.Forms.Cursors.AppStarting
+        Me.rna.Font = New System.Drawing.Font("Verdana", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.rna.ForeColor = System.Drawing.Color.MediumBlue
+        Me.rna.Location = New System.Drawing.Point(450, 198)
+        Me.rna.Name = "rna"
+        Me.rna.Size = New System.Drawing.Size(314, 296)
+        Me.rna.TabIndex = 3
+        Me.rna.Text = ""
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.Color.Black
+        Me.Panel1.Controls.Add(Me.lyrics)
+        Me.Panel1.Controls.Add(Me.Label6)
+        Me.Panel1.Controls.Add(Me.Label5)
+        Me.Panel1.Controls.Add(Me.Label3)
+        Me.Panel1.Controls.Add(Me.Label4)
+        Me.Panel1.Controls.Add(Me.dna)
+        Me.Panel1.Controls.Add(Me.Button1)
+        Me.Panel1.Controls.Add(Me.Label2)
+        Me.Panel1.Controls.Add(Me.rna)
+        Me.Panel1.Controls.Add(Me.Label1)
+        Me.Panel1.Controls.Add(Me.PictureBox1)
+        Me.Panel1.Controls.Add(Me.Panel3)
+        Me.Panel1.Controls.Add(Me.Panel4)
+        Me.Panel1.Controls.Add(Me.Panel5)
+        Me.Panel1.Location = New System.Drawing.Point(12, 12)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(1204, 553)
+        Me.Panel1.TabIndex = 6
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Rockwell", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.ForeColor = System.Drawing.Color.White
+        Me.Label6.Location = New System.Drawing.Point(935, 165)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(18, 19)
+        Me.Label6.TabIndex = 12
+        Me.Label6.Text = "0"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Rockwell", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.ForeColor = System.Drawing.Color.White
+        Me.Label5.Location = New System.Drawing.Point(549, 161)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(18, 19)
+        Me.Label5.TabIndex = 11
+        Me.Label5.Text = "0"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Rockwell", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.ForeColor = System.Drawing.Color.White
+        Me.Label3.Location = New System.Drawing.Point(110, 161)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(18, 19)
+        Me.Label3.TabIndex = 10
+        Me.Label3.Text = "0"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Sitka Display", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.ForeColor = System.Drawing.Color.LawnGreen
+        Me.Label4.Location = New System.Drawing.Point(821, 156)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(114, 30)
+        Me.Label4.TabIndex = 8
+        Me.Label4.Text = "DNA Value:"
+        '
+        'dna
+        '
+        Me.dna.Font = New System.Drawing.Font("Verdana", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.dna.ForeColor = System.Drawing.Color.DarkViolet
+        Me.dna.Location = New System.Drawing.Point(833, 198)
+        Me.dna.Name = "dna"
+        Me.dna.Size = New System.Drawing.Size(314, 296)
+        Me.dna.TabIndex = 7
+        Me.dna.Text = ""
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.Red
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.ForeColor = System.Drawing.Color.White
+        Me.Button1.Location = New System.Drawing.Point(1172, 3)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(29, 21)
+        Me.Button1.TabIndex = 6
+        Me.Button1.Text = "X"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.UCAG_Translator.My.Resources.Resources.Untitled_1
+        Me.PictureBox1.Location = New System.Drawing.Point(65, 18)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(1074, 160)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize
+        Me.PictureBox1.TabIndex = 9
+        Me.PictureBox1.TabStop = False
+        '
+        'Panel3
+        '
+        Me.Panel3.BackColor = System.Drawing.Color.Gray
+        Me.Panel3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Panel3.Location = New System.Drawing.Point(57, 189)
+        Me.Panel3.Name = "Panel3"
+        Me.Panel3.Size = New System.Drawing.Size(333, 314)
+        Me.Panel3.TabIndex = 14
+        '
+        'Panel4
+        '
+        Me.Panel4.BackColor = System.Drawing.Color.Gray
+        Me.Panel4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Panel4.Location = New System.Drawing.Point(441, 189)
+        Me.Panel4.Name = "Panel4"
+        Me.Panel4.Size = New System.Drawing.Size(333, 314)
+        Me.Panel4.TabIndex = 15
+        '
+        'Panel5
+        '
+        Me.Panel5.BackColor = System.Drawing.Color.Gray
+        Me.Panel5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Panel5.Location = New System.Drawing.Point(824, 189)
+        Me.Panel5.Name = "Panel5"
+        Me.Panel5.Size = New System.Drawing.Size(333, 314)
+        Me.Panel5.TabIndex = 16
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Sitka Small", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.ForeColor = System.Drawing.Color.Black
+        Me.Label7.Location = New System.Drawing.Point(1088, 554)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(128, 16)
+        Me.Label7.TabIndex = 13
+        Me.Label7.Text = "  The Survivors 2018"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Gold
+        Me.ClientSize = New System.Drawing.Size(1229, 578)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Panel1)
+        Me.Cursor = System.Windows.Forms.Cursors.IBeam
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "UCAG Translator"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents lyrics As System.Windows.Forms.RichTextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents rna As System.Windows.Forms.RichTextBox
+    Friend WithEvents Panel1 As System.Windows.Forms.Panel
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents dna As System.Windows.Forms.RichTextBox
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents Panel3 As System.Windows.Forms.Panel
+    Friend WithEvents Panel4 As System.Windows.Forms.Panel
+    Friend WithEvents Panel5 As System.Windows.Forms.Panel
+
+End Class
---
+++ UCAG Translator/Form1.vb
@@ -0,0 +1,288 @@
+﻿Public Class Form1
+    Dim check As String
+
+    Private Sub lyrics_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles lyrics.TextChanged
+        Dim lyr As String
+        Dim ic As Integer
+        Dim tmp As Integer
+        rna.Text = ""
+        dna.Text = ""
+
+        lyr = lyrics.Text
+        check = lyrics.Text
+
+        'RNA Values
+        Dim codonA() As String = {" ", "CGA ", "CGG ", "CGT ", "CGC "}
+        Dim codonB = " [b] "
+        Dim codonC() As String = {" ", "ACA ", "ACG "}
+        Dim codonD() As String = {" ", "CTA ", "CTG "}
+        Dim codonE() As String = {" ", "CTT ", "CTC "}
+        Dim codonF() As String = {" ", "AAA ", "AAG "}
+        Dim codonG() As String = {" ", "CCA ", "CCG ", "CCT ", "CCC "}
+        Dim codonH() As String = {" ", "GTA ", "GTG "}
+        Dim codonI() As String = {" ", "TAA ", "TAG ", "TAT "}
+        Dim codonJ = " [j] "
+        Dim codonK() As String = {" ", "TTT ", "TTC "}
+        Dim codonL() As String = {" ", "AAT ", "AAC ", "GAA ", "GAG ", "GAT ", "GAC "}
+        Dim codonM = "TAC "
+        Dim codonN() As String = {" ", "TTA ", "TTG "}
+        Dim codonO = " [o] "
+        Dim codonP() As String = {" ", "GGA ", "GGG ", "GGT ", "GGC "}
+        Dim codonQ() As String = {" ", "GTT ", "GTC "}
+        Dim codonR() As String = {" ", "GCA ", "GCG ", "GCT ", "GCC ", "TCT ", "TCC "}
+        Dim codonS() As String = {" ", "AGA ", "AGG ", "AGT ", "AGC ", "TCA ", "TCG "}
+        Dim codonT() As String = {" ", "TGA ", "TGG ", "TGT ", "TGC "}
+        Dim codonU = " [u] "
+        Dim codonV() As String = {" ", "CAA ", "CAG ", "CAT ", "CAC "}
+        Dim codonW = "ACC "
+        Dim codonX = " [x] "
+        Dim codonY() As String = {" ", "ATA ", "ATG "}
+        Dim codonZ = " [z] "
+        Dim codonSS() As String = {" ", "ATT ", "ATC ", "ACT "}
+
+        'DNA Values
+
+        Dim codonA_() As String = {" ", "GCU ", "GCC ", "GCA ", "GCG "}
+        Dim codonC_() As String = {" ", "UGU ", "UGC "}
+        Dim codonD_() As String = {" ", "GAU ", "GAC "}
+        Dim codonE_() As String = {" ", "GAA ", "GAG "}
+        Dim codonF_() As String = {" ", "UUU ", "UUC "}
+        Dim codonG_() As String = {" ", "GGU ", "GGC ", "GGA ", "GGG "}
+        Dim codonH_() As String = {" ", "CAU ", "CAC "}
+        Dim codonI_() As String = {" ", "AUU ", "AUC ", "AUA "}
+        Dim codonK_() As String = {" ", "AAA ", "AAG "}
+        Dim codonL_() As String = {" ", "UUA ", "UUG ", "CUU ", "CUC ", "CUA ", "CUG "}
+        Dim codonM_ = "AUG "
+        Dim codonN_() As String = {" ", "AAU ", "AAC "}
+        Dim codonP_() As String = {" ", "CCU ", "CCC ", "CCA ", "CCG "}
+        Dim codonQ_() As String = {" ", "CAA ", "CAG "}
+        Dim codonR_() As String = {" ", "CGU ", "CGC ", "CGA ", "CGG ", "AGA ", "AGG "}
+        Dim codonS_() As String = {" ", "UCU ", "UCC ", "UCA ", "UCG ", "AGU ", "AGC "}
+        Dim codonT_() As String = {" ", "ACU ", "ACC ", "ACA ", "ACG "}
+        Dim codonV_() As String = {" ", "GUU ", "GUC ", "GUA ", "GUG "}
+        Dim codonW_ = "UGG "
+        Dim codonY_() As String = {" ", "UAU ", "UAC "}
+        Dim codonSS_() As String = {" ", "UAA ", "UAG ", "UGA "}
+
+
+        'Special Characters
+        Dim codonSSS As String = " [']  "
+        Dim codonQM As String = " [?]  "
+        Dim codonQT As String = " [,]  "
+        Dim codonHT As String = " [!]  "
+        Dim codonPoint As String = " [.]  "
+        Dim codonHY As String = " [-]  "
+
+        For ic = 0 To lyr.Length - 1
+
+            'UCAG VALUES
+            If lyr.Chars(ic).ToString.ToUpper = "A" Then
+                tmp = CInt(Math.Ceiling(Rnd() * 4))
+                rna.Text = rna.Text + codonA(tmp)
+                dna.Text = dna.Text + codonA_(tmp)
+            End If
+
+            If lyr.Chars(ic).ToString.ToUpper = "B" Then
+                rna.Text = rna.Text + codonB
+                dna.Text = dna.Text + codonB
+            End If
+
+            If lyr.Chars(ic).ToString.ToUpper = "C" Then
+                tmp = CInt(Math.Ceiling(Rnd() * 2))
+                rna.Text = rna.Text + codonC(tmp)
+                dna.Text = dna.Text + codonC_(tmp)
+            End If
+
+            If lyr.Chars(ic).ToString.ToUpper = "D" Then
+                tmp = CInt(Math.Ceiling(Rnd() * 2))
+                rna.Text = rna.Text + codonD(tmp)
+                dna.Text = dna.Text + codonD_(tmp)
+            End If
+
+            If lyr.Chars(ic).ToString.ToUpper = "E" Then
+                tmp = CInt(Math.Ceiling(Rnd() * 2))
+                rna.Text = rna.Text + codonE(tmp)
+                dna.Text = dna.Text + codonE_(tmp)
+            End If
+
+            If lyr.Chars(ic).ToString.ToUpper = "F" Then
+                tmp = CInt(Math.Ceiling(Rnd() * 2))
+                rna.Text = rna.Text + codonF(tmp)
+                dna.Text = dna.Text + codonF_(tmp)
+            End If
+
+            If lyr.Chars(ic).ToString.ToUpper = "G" Then
+                tmp = CInt(Math.Ceiling(Rnd() * 4))
+                rna.Text = rna.Text + codonG(tmp)
+                dna.Text = dna.Text + codonG_(tmp)
+            End If
+
+            If lyr.Chars(ic).ToString.ToUpper = "H" Then
+                tmp = CInt(Math.Ceiling(Rnd() * 2))
+                rna.Text = rna.Text + codonH(tmp)
+                dna.Text = dna.Text + codonH_(tmp)
+            End If
+
+            If lyr.Chars(ic).ToString.ToUpper = "I" Then
+                tmp = CInt(Math.Ceiling(Rnd() * 3))
+                rna.Text = rna.Text + codonI(tmp)
+                dna.Text = dna.Text + codonI_(tmp)
+            End If
+
+            If lyr.Chars(ic).ToString.ToUpper = "J" Then
+                rna.Text = rna.Text + codonJ
+                dna.Text = dna.Text + codonJ
+            End If
+
+            If lyr.Chars(ic).ToString.ToUpper = "K" Then
+                tmp = CInt(Math.Ceiling(Rnd() * 2))
+                rna.Text = rna.Text + codonK(tmp)
+                dna.Text = dna.Text + codonK_(tmp)
+            End If
+
+            If lyr.Chars(ic).ToString.ToUpper = "L" Then
+                tmp = CInt(Math.Ceiling(Rnd() * 6))
+                rna.Text = rna.Text + codonL(tmp)
+                dna.Text = dna.Text + codonL_(tmp)
+            End If
+
+            If lyr.Chars(ic).ToString.ToUpper = "M" Then
+                rna.Text = rna.Text + codonM
+                dna.Text = dna.Text + codonM_
+
+            End If
+
+            If lyr.Chars(ic).ToString.ToUpper = "N" Then
+                tmp = CInt(Math.Ceiling(Rnd() * 2))
+                rna.Text = rna.Text + codonN(tmp)
+                dna.Text = dna.Text + codonN_(tmp)
+            End If
+
+            If lyr.Chars(ic).ToString.ToUpper = "O" Then
+                rna.Text = rna.Text + codonO
+                dna.Text = dna.Text + codonO
+            End If
+
+            If lyr.Chars(ic).ToString.ToUpper = "P" Then
+                tmp = CInt(Math.Ceiling(Rnd() * 4))
+                rna.Text = rna.Text + codonP(tmp)
+                dna.Text = dna.Text + codonP_(tmp)
+            End If
+
+            If lyr.Chars(ic).ToString.ToUpper = "Q" Then
+                tmp = CInt(Math.Ceiling(Rnd() * 2))
+                rna.Text = rna.Text + codonQ(tmp)
+                dna.Text = dna.Text + codonQ_(tmp)
+            End If
+
+            If lyr.Chars(ic).ToString.ToUpper = "R" Then
+                tmp = CInt(Math.Ceiling(Rnd() * 6))
+                rna.Text = rna.Text + codonR(tmp)
+                dna.Text = dna.Text + codonR_(tmp)
+            End If
+
+            If lyr.Chars(ic).ToString.ToUpper = "S" Then
+                tmp = CInt(Math.Ceiling(Rnd() * 6))
+                rna.Text = rna.Text + codonS(tmp)
+                dna.Text = dna.Text + codonS_(tmp)
+            End If
+
+            If lyr.Chars(ic).ToString.ToUpper = "T" Then
+                tmp = CInt(Math.Ceiling(Rnd() * 4))
+                rna.Text = rna.Text + codonT(tmp)
+                dna.Text = dna.Text + codonT_(tmp)
+            End If
+
+            If lyr.Chars(ic).ToString.ToUpper = "U" Then
+                rna.Text = rna.Text + codonU
+                dna.Text = dna.Text + codonU
+
+            End If
+
+            If lyr.Chars(ic).ToString.ToUpper = "V" Then
+                tmp = CInt(Math.Ceiling(Rnd() * 4))
+                rna.Text = rna.Text + codonV(tmp)
+                dna.Text = dna.Text + codonV_(tmp)
+            End If
+
+            If lyr.Chars(ic).ToString.ToUpper = "W" Then
+                rna.Text = rna.Text + codonW
+                dna.Text = dna.Text + codonW_
+            End If
+
+            If lyr.Chars(ic).ToString.ToUpper = "X" Then
+                rna.Text = rna.Text + codonX
+                dna.Text = dna.Text + codonX
+
+            End If
+
+            If lyr.Chars(ic).ToString.ToUpper = "Y" Then
+                tmp = CInt(Math.Ceiling(Rnd() * 2))
+                rna.Text = rna.Text + codonY(tmp)
+                dna.Text = dna.Text + codonY_(tmp)
+            End If
+
+            If lyr.Chars(ic).ToString.ToUpper = "Z" Then
+                rna.Text = rna.Text + codonZ
+                dna.Text = dna.Text + codonZ
+
+            End If
+
+            If lyr.Chars(ic).ToString.ToUpper = " " Then
+                tmp = CInt(Math.Ceiling(Rnd() * 3))
+                rna.Text = rna.Text + codonSS(tmp)
+                dna.Text = dna.Text + codonSS_(tmp)
+            End If
+
+
+
+
+            'special characters
+            If lyr.Chars(ic).ToString.ToUpper = "'" Then
+                rna.Text = rna.Text + codonSSS
+                dna.Text = dna.Text + codonSSS
+            End If
+            If lyr.Chars(ic).ToString.ToUpper = "-" Then
+                rna.Text = rna.Text + codonHY
+                dna.Text = dna.Text + codonHY
+            End If
+
+            If lyr.Chars(ic).ToString.ToUpper = "?" Then
+                rna.Text = rna.Text + codonQM
+                dna.Text = dna.Text + codonQM
+            End If
+
+            If lyr.Chars(ic).ToString.ToUpper = "," Then
+                rna.Text = rna.Text + codonQT
+                dna.Text = dna.Text + codonQT
+            End If
+
+            If lyr.Chars(ic).ToString.ToUpper = "!" Then
+                rna.Text = rna.Text + codonHT
+                dna.Text = dna.Text + codonHT
+            End If
+
+            If lyr.Chars(ic).ToString.ToUpper = "." Then
+                rna.Text = rna.Text + codonPoint
+                dna.Text = dna.Text + codonPoint
+
+            End If
+            If IsNumeric(lyr.Chars(ic)) = True Then
+                rna.Text = rna.Text + "[" + lyr.Chars(ic) + "] "
+                dna.Text = dna.Text + "[" + lyr.Chars(ic) + "] "
+
+            End If
+
+        Next
+        Label3.Text = lyrics.TextLength
+        Label5.Text = rna.TextLength
+        Label6.Text = dna.TextLength
+
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Me.Close()
+    End Sub
+
+
+End Class
